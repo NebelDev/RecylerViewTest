466a9373d729f131b821ba8d5270dddcf4c0dec6
Nebeldev <razheem@libero.it>
2015-04-26 17:17:59 +0200

    -

--- app/src/main/java/com/recylerview/recylerviewtest/MainActivity.java
+++ app/src/main/java/com/recylerview/recylerviewtest/MainActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
